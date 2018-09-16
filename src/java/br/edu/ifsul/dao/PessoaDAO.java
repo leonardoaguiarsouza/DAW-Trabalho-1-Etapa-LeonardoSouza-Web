95403becdc02ddc87aa57ea23cd608495a90accc
Leonardo <Leonardo@Leonardo-PC>
2018-09-16 17:55:00 -0300

    CRUDs implementados: Locatario e Pessoa.

---
+++ src/java/br/edu/ifsul/dao/PessoaDAO.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsul.dao;
+
+import br.edu.ifsul.jpa.EntityManagerUtil;
+import br.edu.ifsul.modelo.Pessoa;
+import br.edu.ifsul.util.Util;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.postgresql.translation.messages_bg;
+
+/**
+ *
+ * @author 201613260113
+ */
+public class PessoaDAO implements Serializable{
+    private String mensagem = "";
+    private EntityManager em;
+    
+    public PessoaDAO(){
+        em = EntityManagerUtil.getEntityManager();
+    }
+
+    public List<Pessoa> getLista(){
+        return em.createQuery(" from Pessoa order by nome").getResultList();
+    }
+    
+    public boolean salvar(Pessoa obj){
+        try {
+            em.getTransaction().begin();
+            if(obj.getId() == null){
+                em.persist(obj);
+            } else {
+                em.merge(obj);
+            }
+            em.getTransaction().commit();
+            mensagem = "Objeto persistido com sucesso!";
+            return true;
+        } catch (Exception e) {
+            if(em.getTransaction().isActive() == false){
+                em.getTransaction().begin();
+            }
+            em.getTransaction().rollback();
+            mensagem = "Erro ao persistir: " + Util.getMensagemErro(e);
+            return false;
+       }
+    }
+    
+    public boolean remover(Pessoa obj){
+        try {
+            em.getTransaction().begin();
+            em.remove(obj);
+            em.getTransaction().commit();
+            mensagem = "Objeto removido com sucesso!";
+            return true;
+        } catch (Exception e) {
+            if(em.getTransaction().isActive() == false){
+                em.getTransaction().begin();
+            }
+            em.getTransaction().rollback();
+            mensagem = "Erro ao persistir: " + Util.getMensagemErro(e);
+            return false;
+       }
+    }
+    
+    public Pessoa localizar(Object id){
+        return em.find(Pessoa.class, id);
+    }
+    
+    /**
+     * @return the mensagem
+     */
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    /**
+     * @param mensagem the mensagem to set
+     */
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    /**
+     * @return the em
+     */
+    public EntityManager getEm() {
+        return em;
+    }
+
+    /**
+     * @param em the em to set
+     */
+    public void setEm(EntityManager em) {
+        this.em = em;
+    }
+    
+    
+}
